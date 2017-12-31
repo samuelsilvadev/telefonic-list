39cb3e0b115e27df1d3742e16936267f1ebbb671
Samuel Silva <samuelsilvawb@gmail.com>
2017-12-31 20:42:29 -0300

    refactor : Definir controller usando nomeaçao de controller na view
    - Definido nome para controller na view
    - Criado variavel local no controller para referenciar o this

--- js/controllers/listaTelefonicaCtrl.js
+++ js/controllers/listaTelefonicaCtrl.js
@@ -5,9 +5,11 @@ angular
 		contactsAPI,
 		operatorsAPI,
 		serialGenerator) {
-		$scope.titleApp = 'Lista Telefonica';
-		$scope.contatos = [];
-		$scope.operadoras = [];
+		log($scope);
+		const listaTelefonica = this;
+		listaTelefonica.titleApp = 'Lista Telefonica';
+		listaTelefonica.contatos = [];
+		listaTelefonica.operadoras = [];
 
 		/**
 		 * carregar Contatos
@@ -15,31 +17,32 @@ angular
 		 */
 		(function init() {
 			contactsAPI.getContacts()
-				.then((response) => $scope.contatos = response.data)
+				.then((response) => listaTelefonica.contatos = response.data)
 				.catch((err) => log(err));
 
 			operatorsAPI.getOperators()
-				.then((response) => $scope.operadoras = response.data)
+				.then((response) => listaTelefonica.operadoras = response.data)
 				.catch((err) => log(err));
 		})();
 
-		$scope.adicionarContato = function(contato) {
+		listaTelefonica.adicionarContato = function(contato) {
 			contato.selecionado = false;
 			contato.data = new Date();
 			contactsAPI.saveContact(contato)
 				.then((response) => {
-					$scope.contatos.push(contato);
-					delete $scope.contato;
-					$scope.contatoForm.$setPristine();
+					listaTelefonica.contatos.push(contato);
+					delete listaTelefonica.contato;
+					listaTelefonica.contatoForm.$setPristine();
 				})
 				.catch((err) => log(err));
 		};
 
-		$scope.removerContatos = function(contatos) {
-			$scope.contatos = contatos.filter((contato) => !contato.selecionado);
+		listaTelefonica.removerContatos = function(contatos) {
+			listaTelefonica.contatos = contatos
+				.filter((contato) => !contato.selecionado);
 		};
 
-		$scope.isContatoSelecionado = function(contatos) {
+		listaTelefonica.isContatoSelecionado = function(contatos) {
 			log(contatos);
 			return contatos.some((contato) => contato.selecionado);
 		};
