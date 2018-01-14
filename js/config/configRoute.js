2df38f9b7263835749df568b8d9972c508096b62
Samuel Silva <samuelsilvawb@gmail.com>
2018-01-14 14:20:59 -0300

    feat : Criar rota para contatos
    - Definido path para visualizar todos os contatos

--- js/config/configRoute.js
+++ js/config/configRoute.js
@@ -1,6 +1,11 @@
 angular
 	.module('listaTelefonica')
 	.config(function($routeProvider) {
-
+		$routeProvider.when('/contatos', {
+			templateUrl: 'view/contatos.html',
+			controller: 'listaTelefonicaCtrl as listaTelefonica',
+		}).otherwise({
+			redirectTo: '/index.html',
+		});
 	});
 
